9e433ec8811a5bf165968c411ed8030788f4a356
vainila <retroman142@gmail.com>
2021-10-10 01:41:44 +0300

    add first page

---
+++ js/app.js
@@ -0,0 +1,31 @@
+let header = document.querySelector(".header");
+let headerBtnContainer = document.querySelector(".navBtn-container");
+
+header.addEventListener("click", headerHander);
+function headerHander(e) {
+   if (
+      e.target.classList.contains("line") ||
+      e.target.classList.contains("header__navBtn")
+   ) {
+      headerBtnContainer.classList.toggle("header__navBtn--active");
+   }
+}
+
+
+
+let heroInputs = document.querySelectorAll(".hero__input");
+heroInputs.forEach(input => {
+   input.addEventListener("focus", inputFocus);
+   input.addEventListener("blur", inputBlur)
+});
+
+function inputFocus(e){
+   document.querySelector(`label[for=${e.target.id}]`).classList.add("hero__label--active");   
+}
+
+function inputBlur(e){
+   if(!e.target.value){
+      document.querySelector(`label[for=${e.target.id}]`).classList.remove("hero__label--active");
+   }
+}
+
