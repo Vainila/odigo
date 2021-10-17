b8d6ef9caecbe7f222e6fb695d787596133509e3
vainila <retroman142@gmail.com>
2021-10-17 21:26:07 +0300

    minor updates

--- js/app.js
+++ js/app.js
@@ -11,21 +11,34 @@ function headerHander(e) {
    }
 }
 
-
-
 let heroInputs = document.querySelectorAll(".hero__input");
-heroInputs.forEach(input => {
+heroInputs.forEach((input) => {
    input.addEventListener("focus", inputFocus);
-   input.addEventListener("blur", inputBlur)
+   input.addEventListener("blur", inputBlur);
 });
 
-function inputFocus(e){
-   document.querySelector(`label[for=${e.target.id}]`).classList.add("hero__label--active");   
+function inputFocus(e) {
+   document
+      .querySelector(`label[for=${e.target.id}]`)
+      .classList.add("hero__label--active");
 }
 
-function inputBlur(e){
-   if(!e.target.value){
-      document.querySelector(`label[for=${e.target.id}]`).classList.remove("hero__label--active");
+function inputBlur(e) {
+   if (!e.target.value) {
+      document
+         .querySelector(`label[for=${e.target.id}]`)
+         .classList.remove("hero__label--active");
    }
 }
 
+let timeLine1 = gsap.timeline({
+   scrollTrigger: {
+      trigger: ".main",
+      start: "10%",
+      end: "50%",
+      scrub: 1,
+   },
+});
+
+
+timeLine1.to(".header",  { backgroundColor: "#0f0f0f" });
